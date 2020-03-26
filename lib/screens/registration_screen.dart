00e524cc1a287c49d20f8bcfe8384b9af4869611
Abdullah Deshmukh <abdullahzakir99@gmail.com>
2020-03-27 00:44:59 +0530

    Setup Registration with FirebaseAuth

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -1,6 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_flash_chat/components/rounded_button.dart';
 import 'package:flutter_flash_chat/constants.dart';
+import 'package:flutter_flash_chat/screens/chat_screen.dart';
 
 class RegistrationScreen extends StatefulWidget {
   static const String id = '/register';
@@ -9,6 +11,8 @@ class RegistrationScreen extends StatefulWidget {
 }
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
+  final _auth = FirebaseAuth.instance;
+  String email, password;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,8 +34,11 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
               height: 48.0,
             ),
             TextField(
+              keyboardType: TextInputType.emailAddress,
+              textAlign: TextAlign.center,
               onChanged: (value) {
                 //Do something with the user input.
+                email = value;
               },
               decoration: kInputDecoration.copyWith(
                 hintText: 'Enter your email',
@@ -41,8 +48,11 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
               height: 8.0,
             ),
             TextField(
+              textAlign: TextAlign.center,
+              obscureText: true,
               onChanged: (value) {
                 //Do something with the user input.
+                password = value;
               },
               decoration: kInputDecoration.copyWith(
                 hintText: 'Enter your password',
@@ -55,8 +65,19 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
               tag: 'registerButton',
               child: RoundedButton(
                 buttonColor: Colors.blueAccent,
-                onPressed: () {
-                  //Implement registration functionality.
+                onPressed: () async {
+                  try {
+                    final newUser = await _auth.createUserWithEmailAndPassword(
+                      email: email,
+                      password: password,
+                    );
+
+                    if (newUser != null) {
+                      Navigator.pushNamed(context, ChatScreen.id);
+                    }
+                  } catch (e) {
+                    print(e);
+                  }
                 },
                 buttonText: 'Register',
               ),
